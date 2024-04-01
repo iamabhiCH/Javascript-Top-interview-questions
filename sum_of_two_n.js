b2a966b95f6ca569518795b479fb99ae7006c0da
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-01 11:49:29 +0530

    JavaScript function to calculate the sum of two numbers.

--- sum_of_two_n.js
+++ sum_of_two_n.js
@@ -1,2 +1,12 @@
 // Write a JavaScript function to calculate the sum of two numbers.  
 
+const prompt  = require('prompt-sync')();
+
+let n1 = parseInt(prompt("Enter first number : "));
+let n2 = parseInt(prompt("Enter second number: "));
+
+function sumOfTwoNumbers(a,b){
+    return a+b;
+}
+
+console.log(sumOfTwoNumbers(n1,n2));
