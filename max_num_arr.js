9d50ac6b4cfacfd6c1f9f9d09a6659279bd992c2
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-01 11:55:57 +0530

    JavaScript program to find the maximum number in an array solved successfully.

--- max_num_arr.js
+++ max_num_arr.js
@@ -1,1 +1,12 @@
-// Write a JavaScript program to find the maximum number in an array.
+// Write a Write a JavaScript program to find the maximum number in an array. 
+
+let arr = [89,70,56,123,45,67,764,77,99,340,651];
+let tmp = arr[0];
+
+for(let i = 0; i < arr.length; i++){
+    if (arr[i] > tmp){
+        tmp = arr[i];
+    }
+}
+
+console.log("Maximum number of the array is : ", tmp);
