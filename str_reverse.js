3953f49be256511764c7803c7f40732c686ead9a
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-03 09:33:53 +0530

    Write a JavaScript function to reverse the given string completed.

--- str_reverse.js
+++ str_reverse.js
@@ -1,2 +1,10 @@
 // WAP to reverse thr given string.
 
+const str = "Abhishek Yadav";
+
+function  reverseString(str) {
+    const revStr = str.split('').reverse().join('');
+    return revStr;
+}
+
+console.log(reverseString(str));
