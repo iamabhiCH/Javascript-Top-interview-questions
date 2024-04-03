33ab5709a8ef737a9c9b5cfac285c112262f7e48
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-03 09:48:46 +0530

    Write a JavaScript program to calculate the factorial of a given number completed.

--- factorial.js
+++ factorial.js
@@ -1,2 +1,12 @@
 // Write a JavaScript program to calculate the factorial of a given number. 
 
+const prompt = require("prompt-sync")();
+
+let num = parseInt(prompt("Enter a number to find factorial : "));
+
+let res = 1;
+for(let i = 1; i <= num; i++){
+    res = res * i;
+}
+
+console.log("Factorial of ",num," is : ",res);
