cb46c3e1ab196e070722e6470c8df9692b9541ff
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-03 09:40:07 +0530

    Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers completd.

--- even_arr.js
+++ even_arr.js
@@ -2,3 +2,18 @@
     Write a JavaScript function that takes an array of numbers and 
     returns a new array with only the even numbers. 
 */
+
+let numbArr = [12,3456,31,79,80,44,77,23,29,404,889,9890,456,91];
+
+let evenArr = [];
+
+function findEvenNumbers(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            evenArr.push(arr[i]);
+        }
+    }
+}
+
+findEvenNumbers(numbArr);
+console.log(evenArr);
