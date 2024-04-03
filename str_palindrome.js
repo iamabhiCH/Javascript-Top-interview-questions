01913feeaf308a8ee736a574598fedbc086b7139
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-03 09:29:15 +0530

    Write a JavaScript function to check if a given string is a palindrome completed.

--- str_palindrome.js
+++ str_palindrome.js
@@ -1,4 +1,11 @@
 /* 
      Write a JavaScript function to check if a given string is a palindrome 
-     (reads the same forwards and backwards)
 */
+
+const str = "abcdcba";
+
+function isPalindrome(str){
+    return str === str.split('').reverse().join('');
+}
+
+console.log(isPalindrome(str));
