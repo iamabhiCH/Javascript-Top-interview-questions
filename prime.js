44bcbd3aff1df249aa5e97a447cd33851a374e03
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-04 09:36:58 +0530

    Write a JavaScript function to check if a given number is prime completed.

--- prime.js
+++ prime.js
@@ -1,2 +1,25 @@
 // Write a JavaScript function to check if a given number is prime.
 
+const prompt = require('prompt-sync')();
+
+const num = parseInt(prompt("Enter a number: "));
+
+function isPrime(num){
+    if(num <= 1){
+        return false;
+    }
+
+    for(let i = 2; i <= num/2; i++){
+        if(num  % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+if(isPrime(num)){
+    console.log("Prime number");
+}
+else{
+    console.log("Not a prime number.")
+}
