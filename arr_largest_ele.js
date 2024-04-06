d7a2b03a2c60ab0ecc4ea9a209405526ea2e773e
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-06 10:02:53 +0530

    JavaScript program to find the largest element in a nested array.

---
+++ arr_largest_ele.js
@@ -0,0 +1,23 @@
+// Write a JavaScript program to find the largest element in a nested array. 
+
+const nestedArray = [1, [3, 4], [5, 6, [7, 8, 9]], 2];
+
+function  findLargest(arr) {
+    let arrVal = -Infinity;
+
+    for(let i = 0; i<arr.length;i++){
+        if (Array.isArray(arr[i])){
+            const val = findLargest(arr[i]);
+            if(val > arrVal){
+                arrVal = val;
+            }
+        } else {
+            if(arr[i] > arrVal){
+                arrVal = arr[i];
+            }
+        }
+    }
+    return arrVal;
+}
+
+console.log("largest element : ", findLargest(nestedArray));
