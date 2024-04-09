e98a277ac64f3af6bf16daa50a758cd0d4aa36d4
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-09 11:41:05 +0530

    Write a JavaScript program to convert a string to title case (capitalize the first letter of each word) completed.

--- string_to_title_case.js
+++ string_to_title_case.js
@@ -1,3 +1,5 @@
+//Write a JavaScript program to convert a string to title case (capitalize the first letter of each word). 
+
 function convertToTitleCase(str) {
     return str.replace(/\b\w/g, function(char) {
       return char.toUpperCase();
