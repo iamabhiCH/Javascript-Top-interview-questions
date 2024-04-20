e437def1e35de693a95cbcadb589876caeb54c4e
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-04-20 23:45:31 +0530

    Update sum_of_two_n.js 11 45

--- sum_of_two_n.js
+++ sum_of_two_n.js
@@ -5,8 +5,8 @@ const prompt  = require('prompt-sync')();
 let n1 = parseInt(prompt("Enter first number : "));
 let n2 = parseInt(prompt("Enter second number: "));
 
-function sumOfTwoNumbers(a,b){
-    return a+b;
+function sumOfTwoNumbers(num1, num2){
+    return num1 + num2;
 }
 
 console.log(sumOfTwoNumbers(n1,n2));
