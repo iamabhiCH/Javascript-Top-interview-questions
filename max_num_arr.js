b55492a3434927c8f98de9d4398a8b920a416b31
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-05-08 14:59:31 +0530

    Update max_num_arr.js 8 4 24

--- max_num_arr.js
+++ max_num_arr.js
@@ -1,11 +1,11 @@
 // Write a Write a JavaScript program to find the maximum number in an array. 
 
 let arr = [89,70,56,123,45,67,764,77,99,340,651];
-let tmp = arr[0];
+let temp = arr[0];
 
 for(let i = 0; i < arr.length; i++){
-    if (arr[i] > tmp){
-        tmp = arr[i];
+    if (arr[i] > temp){
+        temp = arr[i];
     }
 }
 
