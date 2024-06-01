1febc9b64f336af464286ebcf1c26ce5a810946e
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-06-01 21:25:16 +0530

    Update even_arr.js 01 06 24

--- even_arr.js
+++ even_arr.js
@@ -3,17 +3,17 @@
     returns a new array with only the even numbers. 
 */
 
-let numbArr = [12,3456,31,79,56,23451,1190,5678,80,44,77,23,29,404,889,9890,456,91];
+let numberArr = [12,3456,31,79,56,23451,1190,5678,80,44,77,23,29,404,889,9890,456,91];
 
-let evenArr = [];
+let eArr = [];
 
 function findEvenNumbers(arr){
     for(let i = 0; i < arr.length; i++){
         if(arr[i] % 2 === 0){
-            evenArr.push(arr[i]);
+            eArr.push(arr[i]);
         }
     }
 }
 
-findEvenNumbers(numbArr);
-console.log(evenArr);
+findEvenNumbers(numberArr);
+console.log(eArr);
